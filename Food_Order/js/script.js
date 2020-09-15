c29e1eb97aa82d84d5ceac720887d78ba5c7d0d5
Hairo <hairo.f@yandex.ru>
2020-09-15 17:09:08 +0500

    Added script for modal window

--- Food_Order/js/script.js
+++ Food_Order/js/script.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
-  
+   
+    // Tabs
+
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -33,6 +35,94 @@ window.addEventListener('DOMContentLoaded', function() {
                     showTabContent();
                 }
             });
+        }
+    });
+
+    //Timer
+
+    const deadLine = '2020-09-30';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor( t / (1000 * 60 *60 * 24) ), /* сколько дней осталось до даты */
+              hours = Math.floor( (t / (1000 * 60 * 60) % 24) ),
+              minutes = Math.floor( (t / 1000 / 60) % 60),
+              seconds = Math.floor( (t / 1000) % 60);
+              
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getTwoDigits(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timerInterval = setInterval(updateClock, 1000);
+        
+        updateClock(); //запуск таймера без задержки
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.textContent = getTwoDigits(t.days),
+            hours.textContent = getTwoDigits(t.hours),
+            minutes.textContent = getTwoDigits(t.minutes),
+            seconds.textContent = getTwoDigits(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timerInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadLine);
+
+    // Modal window
+
+    const modalWindow = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          closeModalBtn = document.querySelector('[data-close]');
+
+    modalWindow.forEach( (btn) => {
+        btn.addEventListener('click', () => {
+            modal.classList.toggle('show');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+    
+    function closeModal() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    }
+
+    closeModalBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (event) => { // close modal with event.target
+        let target = event.target;
+
+        if (target.classList.contains('modal')) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (event) => {
+        if (event.code == 'Escape' && modal.classList.contains('show')) {
+            closeModal();
         }
     });
 });
