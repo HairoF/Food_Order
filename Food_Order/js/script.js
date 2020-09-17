4366c7208a3f8352895fbf6b1e0b5b609c8ac839
Hairo <hairo.f@yandex.ru>
2020-09-17 17:13:22 +0500

    Fixed mistakes

--- Food_Order/js/script.js
+++ Food_Order/js/script.js
@@ -129,7 +129,7 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    //const modalTimerId = setTimeout(openModal, 8000);
+    const modalTimerId = setTimeout(openModal, 10000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -198,27 +198,39 @@ window.addEventListener('DOMContentLoaded', function() {
         postData(item);
     });
 
-    function postDate(form) {
+    function postData(form) {
         form.addEventListener('submit', (event) => {
             event.preventDefault();
 
-            const statusMessage = document.createElement('div');
+            let statusMessage = document.createElement('div');
             statusMessage.classList.add('status');
             statusMessage.textContent = message.loading;
             form.append(statusMessage);
 
+
             const request = new XMLHttpRequest();
             request.open('POST', './server.php');
-            
-            //request.setRequestHeader('Content-type', 'multipart/from-date');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
             const formData = new FormData(form);
 
-            request.send(formData);
+            const object = {};
+            formData.forEach( function(value,key) {
+                object[key] = value; 
+            });
+            console.log(object);
+            const json = JSON.stringify(object);
+
+
+            request.send(json);
 
             request.addEventListener('load', () => {
                 if (request.status == 200) {
                     console.log(request.response);
                     statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
                 } else {
                     statusMessage.textContent = message.failure;
                 }
