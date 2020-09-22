ba882e4c7a6a64d4fd6ba069f127e5d1ca6a8f26
Hairo <hairo.f@yandex.ru>
2020-09-22 13:25:44 +0500

    Sending data with fetch API

--- Food_Order/js/script.js
+++ Food_Order/js/script.js
@@ -210,32 +210,31 @@ window.addEventListener('DOMContentLoaded', function() {
             
             form.insertAdjacentElement('afterend', statusMessage);
 
-
-            const request = new XMLHttpRequest();
-            request.open('POST', './server.php');
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
             const formData = new FormData(form);
 
             const object = {};
             formData.forEach( function(value,key) {
                 object[key] = value; 
             });
-            console.log(object);
-            const json = JSON.stringify(object);
 
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status == 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(response => response.text())
+            .then(response => {
+                console.log(response);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            })
+            .catch(() => {
+                showThanksModal(message.failure);
+            })
+            .finally(() => {
+                form.reset();
             });
         });
     }
